bff54fbff7c8fd11fc8c874c86bdc32c5d25e8dd
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-10-20 13:12:17 -0400

    Added Policy Page
    Added Policy Page and Styles

---
+++ privacy.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Alastair's Home Décor Shop - Valery Menelas</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="img/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="img/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="home.php"><img src="img/home/logo.png" alt="Alastair's" /></a>
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="admin.php"><i class="fa fa-lock"></i> Admin</a></li>
+								<li><a href="checkout.php"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="home.php">Home</a></li>
+								<li class="dropdown"><a href="#" class="active">Shop<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.php">Products</a></li>
+										<li><a href="product-details.php">Product Details</a></li> 
+										<li><a href="checkout.php">Checkout</a></li> 
+										<li><a href="cart.php">Cart</a></li> 
+                                    </ul>
+                                </li> 
+								<li><a href="contact-us.php">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Search"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
+	
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" class="menu-active">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Customer Relations
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear" class="panel-collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">About Us</a></li>
+											<li><a href="#">How to Shop</a></li>
+											<li><a href="#">Frequently Asked Questions</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span></span>
+											Policies
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear" class="panel-collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Privacy Policy</a></li>
+											<li><a href="#">Website Terms & Conditions</a></li>
+											<li><a href="#">Copyright & Trademark Notice</a></li>
+											<li><a href="#">Return Policy</a></li>
+											<li><a href="#">Shipping Information</a></li>
+											<li><a href="#">Merchandise Safety/a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div><!--/category-products-->
+						
+					</div>
+				</div>
+				
+				<div class="col-sm-9 padding-right">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Privacy Policy</h2>
+						<h3 class="title"> Introduction and Overview</h3>
+							<p class="paragraph"> You have arrived at a web site that is provided by Alistair's (“Alistair's” or “we,” “our” or “us”). This “Privacy Policy” governs your use of www.alistairs.com and any other official Alistair's sites (including mobile sites or apps), and any interactive features, applications, and/or other services that we make available through a site that posts a link to this Privacy Policy (collectively, referred to as the “Site”). This Privacy Policy also applies to Personal Information (defined below) we may collect from you in our Alistair's retail stores (“Store” or “Stores”), and through official surveys and sweepstakes entry forms, or in any other instance when you contact us.</p>
+							<p class="paragraph">This Privacy Policy describes what information Alistair's collects about you, why we collect it, how we use it, and under what circumstances we share it with third parties. This Privacy Policy also describes the choices you can make about how we use your information. To the extent we provide you notice on our Site or in our Stores of different or additional privacy policies or practices (e.g., at the point of our collection), those additional terms shall govern such data collection and use.</p>
+							<p class="paragraph">In addition, please review the Site’s Terms of Use, which governs your use of the Site. By using our Site, you consent to our Privacy Policy and Terms of Use.</p>
+						
+						<h3 class="title">Information Your Provide To Us</h3>
+							<p class="paragraph">We may ask you to provide us: (1) your first and last name, email address, home address, phone number, driver’s license number, credit and debit card numbers, checking account numbers, date of birth and social security number (when applying for or requesting services related to the Alistair's Rewards Card) or other information that could reasonably be used to identify you personally (hereinafter “Personal Information”); and/or (2) demographic information, such as information like your zip code (“Demographic Information”). We may collect this information from you such as when you:</p>
+								<ul class="privacylist"> 
+									<li>Submit account registration forms</li>
+									<li>Sign up for Alistair's Rewards Card</li>
+									<li>Participate in a survey or promotion</li>
+									<li>Contact us regarding a question or concern</li>
+									<li>and Otherwise interact with the Site or the Store.</li>
+								</ul>
+							<p class="paragraph">Your decision to provide this information is voluntary, but if you choose not to provide this requested information you may not be able to take advantage of all of the Site’s features or our services.</p>	
+						<h3 class="title">Information We Collect When You Use the Site</h3>
+							<p class="paragraph">In addition to information that you choose to submit to us, we and/or our service providers may automatically collect and/or store certain information when you visit or interact with the Site (“Usage Information”). This Usage Information may be stored and/or accessed from your personal computer, laptop, tablet, mobile phone or other device (a “Device”) whenever you visit or interact with our Site. Usage Information may include:</p>
+								<ul class="privacylist"> 
+									<li>your IP address or other unique identifier (“Device Identifier”)</li>
+									<li>your Device browser, operating system and mobile network information</li>
+									<li>the areas within our Site that you visit and your activities there, including remembering you and your preferences</li>
+									<li>your general location, and</li>
+									<li>certain other Device data, including the time of day you visit our Site, among other information.</li>
+								</ul>
+							<p class="paragraph">We may use various methods and technologies to collect and/or store Usage Information, such as:</p>	
+
+
+
+					</div><!--main privacy policy-->
+				</div>
+			</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="contact-us.php">Contact Us</a></li>
+								<li><a href="#">FAQ</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>Shop</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Furniture</a></li>
+								<li><a href="#">Art & Wall Décor</a></li>
+								<li><a href="#">Lighting</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privecy Policy</a></li>
+								<li><a href="#">Refund Policy</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>About Us</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">About Us</a></li>
+							</ul>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2015 Alastair's Inc. All rights reserved.</p>
+					<p class="pull-right">This site is not official and is an assignment for a UCF Digital Media course, designed by Valery Menelas</p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <script src="js/jquery.js"></script>
+	<script src="js/price-range.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
