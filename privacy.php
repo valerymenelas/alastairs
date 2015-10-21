8e493aff3e60afdae2d622ed305d6ae7706ba44c
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-10-20 23:56:41 -0400

    Updated Privacy Page and Style.css
    updated page.

--- privacy.php
+++ privacy.php
@@ -95,7 +95,7 @@
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" class="menu-active">
+										<a data-toggle="collapse" data-parent="#accordian">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
 											Customer Relations
 										</a>
@@ -114,7 +114,7 @@
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian">
+										<a data-toggle="collapse" data-parent="#accordian" class="menu-active">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span></span>
 											Policies
 										</a>
@@ -123,7 +123,7 @@
 								<div id="sportswear" class="panel-collapse">
 									<div class="panel-body">
 										<ul>
-											<li><a href="#">Privacy Policy</a></li>
+											<li><a href="#" class="menu-active">Privacy Policy</a></li>
 											<li><a href="#">Website Terms & Conditions</a></li>
 											<li><a href="#">Copyright & Trademark Notice</a></li>
 											<li><a href="#">Return Policy</a></li>
@@ -146,7 +146,7 @@
 							<p class="paragraph">This Privacy Policy describes what information Alistair's collects about you, why we collect it, how we use it, and under what circumstances we share it with third parties. This Privacy Policy also describes the choices you can make about how we use your information. To the extent we provide you notice on our Site or in our Stores of different or additional privacy policies or practices (e.g., at the point of our collection), those additional terms shall govern such data collection and use.</p>
 							<p class="paragraph">In addition, please review the Site’s Terms of Use, which governs your use of the Site. By using our Site, you consent to our Privacy Policy and Terms of Use.</p>
 						
-						<h3 class="title">Information Your Provide To Us</h3>
+						<h3 class="title">Information You Provide To Us</h3>
 							<p class="paragraph">We may ask you to provide us: (1) your first and last name, email address, home address, phone number, driver’s license number, credit and debit card numbers, checking account numbers, date of birth and social security number (when applying for or requesting services related to the Alistair's Rewards Card) or other information that could reasonably be used to identify you personally (hereinafter “Personal Information”); and/or (2) demographic information, such as information like your zip code (“Demographic Information”). We may collect this information from you such as when you:</p>
 								<ul class="privacylist"> 
 									<li>Submit account registration forms</li>
@@ -166,8 +166,38 @@
 									<li>certain other Device data, including the time of day you visit our Site, among other information.</li>
 								</ul>
 							<p class="paragraph">We may use various methods and technologies to collect and/or store Usage Information, such as:</p>	
-
-
+								<ul class="privacylist">
+									<li><strong>Cookies:</strong> A cookie is a file placed on a Device to uniquely identify your browser or to store information on your Device. Our Site may use HTTP cookies, HTML5 cookies, Flash cookies and other types of cookie technology to store information on local storage. Regular cookies may generally be disabled or removed by tools that are available as part of most commercial browsers, and in some (but not all) instances can be blocked by selecting certain settings. Each browser you use will need to be set separately and different browsers offer different functionality and options. Please note that these tools may not be effective with regard to Flash cookies or HTML5 cookies. For information on disabling Flash cookies, visit www.adobe.com. Please understand that if you disable or remove any type of cookie, some parts of our Site may not function properly.</li>
+									<li><strong>Web Beacons:</strong> A Web Beacon is a small tag (which may be invisible to you) that may be placed on our Site’s pages and messages. Web beacons may be used by us for a number of purposes, including to count visitors to the Site, to monitor how users navigate the Site, to count how many emails that were sent were actually opened or to count how many particular links were actually viewed.</li>
+									<li><strong>Embedded Script:</strong> An embedded script is programming code that is designed to collect information about your interactions with the Site, such as the links you click on. The code is temporarily downloaded onto your Device from our web server or a third party service provider, is active only while you are connected to the Site, and is deactivated or deleted thereafter. The embedded script does not collect your Personal Information.</li>
+									<li><strong>ETag, or entity tag:</strong> An Etag or entity tag is a feature of the cache in browsers. It is an opaque identifier assigned by a web server to a specific version of a resource found at a URL. If the resource content at that URL ever changes, a new and different ETag is assigned. Used in this manner ETags are a form of Device Identifier. ETag tracking may generate unique tracking values even where the consumer blocks HTTP, Flash and/or HTML5 cookies.</li>
+								</ul>
+						<h3 class="title">Location-Based Information</h3>
+							<p class="paragraph">Our Site (including our mobile apps) may use location-based services to verify your location, deliver you relevant content and/or advertising based on your location, as well as to share your location with our service providers as part of the location-based services we offer. Further, we or our service providers may use your location information to display our nearest Store location to you. You have the ability to turn location-based services on and off by adjusting the settings of your Internet browser or mobile device, or by following the standard uninstall process and removing all Pier 1 Imports applications from your mobile device.</p>		
+						<h3 class="title">Information We Collect From Third Parties</h3>
+							<p class="paragraph">We may also receive information about you from your friends and other third parties, and combine or link that information with information we have about you. To the extent we combine information we receive from third parties with your Personal Information, the combined information, unless disclosed otherwise, will be treated as Personal Information under this Privacy Policy. In no other circumstances will this Privacy Policy apply to information we receive about you from third parties.</p>		
+						<h3 class="title">Information We Collect When You Interact with Third-Party Sites </h3>
+							<p class="paragraph">The Site may allow certain kinds of interactions between the Site and your account on a third-party site or application. The use of this functionality may involve the third-party site providing information, including Personal Information, to us. For example, when you register with the Site, you may have an option to use your Facebook account to facilitate the registration or transaction process on the Site. If you choose to use this functionality, the third-party site may send Personal Information about you to us. If so, we will treat this information as Personal Information under this Privacy Policy. In addition, we may provide links on the Site to facilitate sending a communication from the Site. For example, we may use third parties to facilitate emails, Tweets or Facebook postings. These third parties may retain any information used or provided in any such communications or activities and these third parties’ practices are not subject to our Privacy Policy. You should review the applicable third-party privacy policies before using such tools.</p>	
+						<h3 class="title">Information You Provide About A Third-Party</h3>
+							<p class="paragraph">You may send someone else a communication from the Site, such as sending an invitation to a friend. If so, the information you provide (names, email addresses, mobile number, etc.) will be used to facilitate the communication and will not be used for marketing purposes unless we obtain consent from that person or we explicitly say otherwise. Please be aware that when you use any send-to-a-friend functionality on our Site, your email address, mobile number, name or username and message may be included in the communication sent to your addressee(s). Some of these tools may be third-party tools subject to third-party privacy policies.</p>		
+						<h3 class="title">How We Use The Information We Collect</h3>
+							<p class="paragraph">We may use your Personal Information, Demographic Information or Usage Information for various purposes, including, but not limited to, the following:</p>
+								<ul class="privacylist"> 
+									<li>Improving the effectiveness of our Site, marketing endeavors and service offerings</li>
+									<li>Processing credit applications</li>
+									<li>Site Registration</li>
+									<li>Processing and completing purchase, return and similar transactions</li>
+									<li>Providing Customer Service</li>
+									<li>Notifying you of product recalls or providing other information concerning products you have purchased</li>
+									<li>Sending you direct mail offerings</li>
+									<li>Sending you email communications such as electronic newsletters about our products, services and Promotions</li>
+									<li>Sending you email about orders you have made on Alistairs.com</li>
+									<li>Enabling you to participate in the Site’s features such as surveys and Promotions</li>
+									<li>Identifying your product and service preferences and informing you of new or additional products, services and Promotions that may be of interest to you</li>
+									<li>Providing mobile marketing messages and service messages</li>
+									<li>For internal business purposes and</li>
+									<li>For purposes disclosed at the time you provide your information or as otherwise set forth in this Privacy Policy.</li>
+								</ul>
 
 					</div><!--main privacy policy-->
 				</div>
