4c17bdb8defb7e1bcbed2d6cfb0f99701f879734
shaylatime <shaylahart@knights.ucf.edu>
2015-10-24 18:26:07 -0400

    Contact Page
    Updated. Will add what everyone has done when site is finished.

--- contact-us.php
+++ contact-us.php
@@ -1,10 +1,101 @@
-<?php include ("includes/header.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Alastair's Home Décor Contact- Valery Menelas</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="img/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="img/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>	
+	<header id="header"><!--header-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="home.php"><img src="img/home/logo.png" alt="Alastair's" /></a>
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="admin.php"><i class="fa fa-lock"></i> Admin</a></li>
+								<li><a href="checkout.php"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="home.php" class="active">Home</a></li>
+								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.php">Products</a></li>
+										<li><a href="product-details.php">Product Details</a></li> 
+										<li><a href="checkout.php">Checkout</a></li> 
+										<li><a href="cart.php">Cart</a></li> 
+                                    </ul>
+                                </li> 
+								<li><a href="contact-us.php">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Search"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
 	 
 	 <div id="contact-page" class="container">
     	<div class="bg">	
     		<div class="row">  	
 	    		<div class="col-sm-8">
-	    			<div class="contact-form">
+                <h2 class="title text-center">About Us</h2>
+                <p> Alastair's strives to provide you with the best products that will enhance the feeling of your home. All of our products come with a warranty and we guarantee that each product is created with precision and care.</p>
+
+<p>Alastair's is located in East Orlando off of University Blvd. In our building we have a showcase where you can see some of the products that we sell. When you come into our store you will be able to order any item and have them delivered to you, or you can conveniently use our site.
+
+</p> 
+                    <div class="contact-form">
 	    				<h2 class="title text-center">Get In Touch</h2>
 	    				<div class="status alert alert-success" style="display: none"></div>
 				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
@@ -18,7 +109,7 @@
 				                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
 				            </div>
 				            <div class="form-group col-md-12">
-				                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+				                <textarea name="message" id="message" required class="form-control" rows="8" placeholder="Your Message Here"></textarea>
 				            </div>                        
 				            <div class="form-group col-md-12">
 				                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
@@ -27,7 +118,7 @@
 	    			</div>
 	    		</div>
 	    		<div class="col-sm-4">
-	    			<div class="contact-info">
+    			  <div class="contact-info">
 	    				<h2 class="title text-center">Contact Info</h2>
 	    				<address>
 	    					<p>Alastair’s Home Décor </p>
@@ -37,6 +128,17 @@
 							<p>Email: info@alastairshome.com</p>
 	    				</address>
 	    			</div>
+                    
+                    <ul>
+	    			  <h2 class="title text-center">Creators</h2>
+	    			<li>Valery Menelas</li>
+                    <li>Timothy Kennedy</li>
+                    <li>Nicholas Hursh</li>
+                    <li>Shayla Hart</li>
+                    <li>Jackeline Baez</li>
+                    <li>Joseph Agudelo</li>
+                    </ul>
+                    
     			</div>    			
 	    	</div>  
     	</div>	
@@ -60,6 +162,7 @@
 						<div class="single-widget">
 							<h2>Shop</h2>
 							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Furniture</a></li>
 								<li><a href="#">Art & Wall Décor</a></li>
 								<li><a href="#">Lighting</a></li>
 							</ul>
