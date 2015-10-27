1c4dc344cfca362a0f8ccf099227652c67a3f35b
valerymenelas <valery.menelas@gmail.com>
2015-10-27 14:01:01 -0400

    menu changes

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h4 class="panel-title"><a href="art_wall_decor.php">Art & Wall Décor</a></h4>
+									<h4 class="panel-title"><a href="art-wall-decor.php">Art & Wall Décor</a></h4>
 								</div>
 							</div>
 							
