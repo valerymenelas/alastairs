40e6d550e74bfd1e0884a205c9508f0263e3a579
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-10-27 13:15:02 -0400

    Policy Pages Updated
    Policy Pages updated with proper name

--- tax-policy.php
+++ tax-policy.php
@@ -124,9 +124,9 @@
 									<div class="panel-body">
 										<ul>
 											<li><a href="privacy-policy.php">Privacy Policy</a></li>
-											<li><a href="return-policy.php" class="menu-active">Return Policy</a></li>
+											<li><a href="return-policy.php">Return Policy</a></li>
 											<li><a href="shipping-policy.php">Shipping Information</a></li>
-											<li><a href="tax-policy.php">Tax Policy</a></li>
+											<li><a href="tax-policy.php" class="menu-active">Tax Policy</a></li>
 										</ul>
 									</div>
 								</div>
@@ -138,48 +138,70 @@
 				
 				<div class="col-sm-9 padding-right">
 					<div class="features_items"><!--features_items-->
-					  <h2 class="title text-center">For Online Purchases</h2>
-						<h3 class="title">Exchanges, Incorrect or Damaged Items</h3>
-							<p class="paragraph"> If you have received an incorrect or damaged item or otherwise need to make an exchange, please call 1-800-555-5555.</p>
-													
-						<h3 class="title">Return via UPS or USPS</h3>
-								<ul class="privacylist"> 
-									<li>Items sent C.O.D. (Cash on Delivery) will not be accepted.</li>
-									<li>To arrange the return of your items, please call 1-800-555-5555.</li>
-									<li>Return shipments may take up to 14 business days to process.</li>
-									<li>Shipping, surcharges, delivery and handling fees are non-refundable.</li>
-								</ul>
+						<h2 class="title text-center">About Sales Tax</h2>
+							<h3 class="title">Items sold by Alastairs.com and shipped to certain states are subject to tax. </h3>
+							<p class="paragraph">Items sold by Amazon.com LLC, or its subsidiaries, and shipped to destinations in the following states are subject to tax:</p>
+							
+							<div class="col-sm-12">
+								<div class="col-md-6">
+									<ul class="shipping-list"> 
+										<li><strong>Order Total</strong><span class="shipping-charge"><strong>Charge</strong></span></li>
+										<li>Arizona</li>
+										<li>California</li>
+										<li>Conneticut</li>
+										<li>Florida</li>
+										<li>Georgia</li>
+										<li>Illinois</li>
+										<li></li>
+										
+									</ul>
+								</div>
+							</div>
 
-						<h3 class="title">Return to Store</h3>
-							<p class="paragraph">You can make a return at any Alastair's store. Please bring the following items with you:</p>
-								<ul class="privacylist"> 
-									<li>The merchandise you’re returning</li>
-									<li>Your receipt or packing slip</li>
-									<li>Your valid photo ID</li>
-									<li>The original form of payment</li>
-								</ul>
-						
-					  <h2 class="title text-center">For Store Purchases</h2>
-							<p class="paragraph">You can make a return at any Alastair's store. Please bring the following items with you:</p>		
-								<ul class="privacylist"> 
-									<li>The merchandise you’re returning</li>
-									<li>Your receipt or packing slip</li>
-									<li>Your valid photo ID</li>
-									<li>The original form of payment</li>
-								</ul>
+						<h2 class="title text-center">How Sales Tax is Calculated</h3>
+							<p class="paragraph">If an item is subject to sales tax in the state to which the order is shipped, tax is generally calculated on the total selling price of each individual item. In accordance with state tax laws, the total selling price of an item will generally include item-level shipping and handling charges, item-level discounts, gift-wrap charges, and an allocation of order-level shipping and handling charges and order-level discounts. </p>
+							<p class="paragraph">The amount of tax charged on your order will depend upon many factors including Identity of the seller, type of item purchased, and destination of the shipment. Factors can change between the time you place an order and the time of credit card charge authorization, which could affect the calculation of sales taxes. The amount appearing on your order as Estimated Tax may differ from the sales taxes ultimately charged. </p>
 
-					  <h2 class="title text-center">Alastair's Return Policy</h2>
-							<p class="paragraph">For merchandise in its original condition, we will provide an exchange or refund in the original form of payment with your original or emailed receipt.</p>
-							<p class="paragraph">If your receipt is older than 45 days, your purchase can be exchanged or we'll issue a store credit for the current selling price.</p>
-							<p class="paragraph">Without a receipt, if you have a valid photo ID, we'll provide an exchange or store credit for the current selling price. This excludes specifically labeled merchandise, which may only be returned with your receipt.</p>
-							<p class="paragraph">For returns of a store purchase, cash refunds are subject to store funds availability and may be paid via mail as a corporate-issued check.</p>
-							<p class="paragraph">Except for seasonal merchandise, we will offer a one-time sale price adjustment, limited to 14 days from purchase date. Original or emailed receipt is required.</p>
-							<p class="paragraph">Return shipping and delivery fees, plus any applicable oversize surcharge fees, will be deducted from the total refund. Shipping, In-Home Delivery, surcharges, delivery and handling fees are non-refundable.</p>
-							<p class="paragraph">Because of the fragile nature of many Pier 1 items, extra care should be taken to ensure that a package arrives at our processing center in the same condition as you received it. Our Customer Relations team can provide detailed packing advice.</p>
-							<p class="paragraph">Return shipments may take up to 14 business days to process.</p>
-							<p class="paragraph">Items sent C.O.D. (Cash on Delivery) will not be received. Please refer to the instructions above for the accepted way to make a return.</p>
-							<p class="paragraph">All sales are final on merchandise purchase “as-is” or with a “damage” discount.</p>		
-					
+
+
+							<table class="table table-striped">
+      <tbody>
+        <tr>
+          <td>Arizona</td>
+          <td>California</td>
+          <td>Conneticut</td>
+          <td>Florida</td>
+          <td>Georgia</td>
+          <td>Illinois</td>
+        </tr>
+        <tr>
+          <td>Indiana</td>
+          <td>Kansas</td>
+          <td>Kentucky</td>
+          <td>Maryland</td>
+          <td>Massachussets</td>
+          <td>Michigan</td>
+        </tr>
+        <tr>
+          <td>Minnesota</td>
+          <td>Nevada</td>
+          <td>New Jersey</td>
+          <td>New York</td>
+          <td>North Carolina</td>
+          <td>North Dakota</td>
+        </tr>
+        <tr>
+          <td>Ohio</td>
+          <td>Pennsylvania</td>
+          <td>Tennesse</td>
+          <td>New York</td>
+          <td>North Carolina</td>
+          <td>North Dakota</td>
+        </tr>
+      </tbody>
+    </table>
+
+
 					</div><!--main privacy policy-->
 				</div>
 			</div>
