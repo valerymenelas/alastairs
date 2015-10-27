667310855906f38de4482cc70395ebec4f9a16db
tkmets5 <timkennedy@Tims-MacBook-Pro.local>
2015-10-27 09:35:53 -0400

    database connection
    I changed this back to Valery’s login info for his database until we
    get our group account.  I will need him to upload an updated sql file
    to it as I needed to add a row for search bar

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
 $db="va634778";
-$user="root";
-$pass="";
+$user="va634778";
+$pass="49C13AD71D0943A9B0E44FD2D7CAA9D0";
 $conn=mysql_connect($host, $user, $pass);
 mysql_select_db($db);
 $query=mysql_query("select * from alastairsgroup_account");
