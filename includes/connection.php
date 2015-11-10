8a88a221e05d94d6f6806bcf385f5eb4db07f9c4
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-11-10 13:46:14 -0500

    updates

--- includes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$db="va634778";
-$user="root";/*va634778*/
-$pass="49C13AD71D0943A9B0E44FD2D7CAA9D0";/*49C13AD71D0943A9B0E44FD2D7CAA9D0*/
-$conn=mysql_connect($host, $user, $pass);
-mysql_select_db($db);
-$query=mysql_query("select * from alastairs");
-?>
