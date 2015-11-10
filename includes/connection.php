ea093199487e7096d787091f86f9707a77e53efa
tkmets5 <timkennedy@Tims-MacBook-Pro.local>
2015-11-10 13:35:54 -0500

    fixed

--- includes/connection.php
+++ includes/connection.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $db="va634778";
 $user="root";/*va634778*/
-$pass="root";/*49C13AD71D0943A9B0E44FD2D7CAA9D0*/
+$pass="49C13AD71D0943A9B0E44FD2D7CAA9D0";/*49C13AD71D0943A9B0E44FD2D7CAA9D0*/
 $conn=mysql_connect($host, $user, $pass);
 mysql_select_db($db);
 $query=mysql_query("select * from alastairs");
