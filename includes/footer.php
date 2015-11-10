ee328768c3cd4ff916110c083ae5acdb1b1f48c8
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-11-10 13:43:22 -0500

    Updated Footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 		<div class="footer-widget">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-3">
+					<div class="col-xs-12 col-md-3">
 						<div class="single-widget">
 							<h2>Customer Service</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -17,7 +17,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
+					<div class="col-xs-12 col-md-3">
 						<div class="single-widget">
 							<h2>Shop</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -30,7 +30,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
+					<div class="col-xs-12 col-md-3">
 						<div class="single-widget">
 							<h2>Our Company</h2>
 							<ul class="nav nav-pills nav-stacked">
@@ -40,7 +40,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
+					<div class="col-xs-12 col-md-3">
 						<div class="single-widget">
 							<h2>Logo Goes Here</h2>
 							<ul class="nav nav-pills nav-stacked">
