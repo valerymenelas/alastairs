089299e25631a7ec2b23cc36971bda6d2e9dba1c
valerymenelas <valerymenelas@gmail.com>
2015-11-10 12:42:08 -0500

    Product details page

--- product-details.php
+++ product-details.php
@@ -1,3 +1,4 @@
+
 <?php include ("includes/header.php"); ?>
 
 		<div class="container">
@@ -71,7 +72,6 @@
 							<div class="product-information"><!--/product-information-->
 								<h2><?php echo $results['productName'];?></h2>
 								<p>SKU: <?php echo $results['sku'];?></p>
-								<img src="img/product-details/rating.png" alt="Rating" /> <br />
 								<span>
 									<span>$<?php echo $results['price'];?></span>
 									<a href="cart.php?prod=<?php echo $results['productID']; ?>" class="btn btn-fefault cart"><i class="fa fa-shopping-cart"> </i> Add to cart</a>
@@ -85,13 +85,100 @@
 						<div class="col-sm-12">
 							<ul class="nav nav-tabs">
 								<li><a href="#details" data-toggle="tab">Details</a></li>
+                                
 							</ul>
-
+ 
 							<p class="descr"><?php echo $results['description'];?></p>
+                         
+                           
 						</div>
 					</div><!--/category-tab-->
-					<?php } ?>
+					
 					<hr />
+                    <!--Rating-->
+                    <div class="row category-tab">
+                   <ul class="nav nav-tabs">
+								<li><a href="#areys" data-toggle="tab">Rating And Review</a></li>
+							</ul>
+                    <form method="post">
+                        <div class="col-sm-4" id="areys">
+                            <p class="descr"> 
+                            <input type="hidden" id="rates" name="rating"/>
+                            <p><label>Rate:</label></p>
+                            <div class='product'>
+       								
+                                <div id="rating_1" class="ratings">
+                                    <div class="star_1 ratings_stars" id="1"></div>
+                                    <div class="star_2 ratings_stars" id="2"></div>
+                                    <div class="star_3 ratings_stars" id="3"></div>
+                                    <div class="star_4 ratings_stars" id="4"></div>
+                                    <div class="star_5 ratings_stars" id="5"></div>
+                                </div>
+                            </div>
+       					 </p>
+                            </div>
+                            <div class="col-sm-4">
+                                
+                        <input type="hidden" value="<?php echo $results['productID']; ?>" name="product_id" />
+                         <p><label>Title</label></p>
+                         <p><input type="text" name="title" /></p>
+                            </div>
+                     <div class="col-sm-4">
+                         <p><label>Description</label></p>
+                         <p><textarea name="product_description"></textarea></p>
+                         <p><input type="submit" value="Submit Review" name="btn_review" /></p>
+                         	</div>
+                          </form> 
+                          </div> 
+                           <?php 
+						   if(isset($_REQUEST["btn_review"]))
+								{
+									$rating=$_REQUEST["rating"];
+									$product_title=$_REQUEST["title"];
+									$product_description=$_REQUEST["product_description"];
+									$product_id=$_REQUEST["product_id"];
+									
+									//$query=mysql_query("UPDATE SET ratings title='$product_title',description='$product_description' WHERE ");
+									$query=mysql_query("INSERT INTO rating_view (title,description,product_id,rate ) 
+									 VALUES ('$product_title','$product_description','$product_id','$rating')") or die(mysql_error());
+									
+									
+								}
+								//echo "SELECT * FORM rating_view WHRE product_id=".$results['productID'];
+					$daat = mysql_query("SELECT * FROM rating_view WHERE product_id=".$results['productID']);
+					?>
+                    <table class="table">
+    <thead>
+      <tr>
+        <th>Rating</th>
+        <th>Title</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+					while($row = mysql_fetch_assoc($daat))
+					{ //print_r($row);
+						?>
+      <tr>
+        <td><?php echo $row['rate'];?></td>
+        <td><?php echo $row['title'];?></td>
+        <td><?php echo $row['description'];?></td>
+      </tr>
+      
+   
+                        <?php
+					}
+						   ?>
+                            </tbody>
+  </table>
+ 
+                            
+						<?php } ?>
+                        
+                        <!--end rating-->
+                    
+                    
 
 					<div class="recommended_items"><!--recommended_items-->
 						<h2 class="title">Similar Products</h2>
