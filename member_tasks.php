629190b714ad862f871a359d27711a9c361bb9ab
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-11-17 00:15:34 -0500

    validation updates

--- member_tasks.php
+++ member_tasks.php
@@ -4,115 +4,99 @@
     	<div class="bg">	
     		<div class="row">  	
 	    		<div class="col-sm-8">
-                <h2 class="title text-center">About Us</h2>
-                <p> Alastair's strives to provide you with the best products that will enhance the feeling of your home. All of our products come with a warranty and we guarantee that each product is created with precision and care.</p>
-				<p>Alastair's is located in East Orlando off of University Blvd. In our building we have a showcase where you can see some of the products that we sell. When you come into our store you will be able to order any item and have them delivered to you, or you can conveniently use our site.</p> 
+                    <h2 class="title text-center">About Us</h2>
+                    <p> Alastair's strives to provide you with the best products that will enhance the feeling of your home. All of our products come with a warranty and we guarantee that each product is created with precision and care.</p>
+    				<p>Alastair's is located in East Orlando off of University Blvd. In our building we have a showcase where you can see some of the products that we sell. When you come into our store you will be able to order any item and have them delivered to you, or you can conveniently use our site.</p> 
+            
                 
-<div class="col-sm-6">
-	<h3>Valery</h3>
-    <ul>
-    	<li> <strong> Original creater of site </strong> </li>
-        <li> PHP back end </li>
-        <li> Interaction with database </li>
-        <li> Guest, Unprivilided, Privileged and Admin access </li>
-        <li> Rating system </li>
-        <li> Site validation </li>
-        <li> CRUD operations </li>
-        <li> Integration of a mobile view </li>
-        <li> General presentation </li>
-        </ul>
-  
-    </div>
-    
-    <div class="col-sm-6">
-	<h3>Tim</h3>
-    <ul>
-    	<li> <strong> Team leader </strong> </li>
-    	<li> PHP back end </li>
-        <li> Integration of site search </li>
-        <li> Interaction with database </li>
-        <li> Guest, Unprivilided, Privileged and Admin access </li>
-        <li> Rating system </li>
-        <li> Site validation </li>
-    	<li> CRUD operations </li>
-        <li> Integration of a mobile view </li>
-        <li> General presentation </li>
-        </ul>
-    
-    </div>
-    
-    <br/>
-    <div class="col-sm-6">
-	<h3>Logan</h3>
-    <ul> 
-    	<li> PHP back end </li>
-    	<li> Interaction with database </li>
-        <li> Site validation </li>
-        <li> CRUD operations </li>
-        <li> Integration of a mobile view </li>
-        <li> General presentation </li>
-        </ul>
- 
-    </div>
-    
-    <div class="col-sm-6">
-	<h3>Jackie</h3>
-    <ul>
-    	<li> PHP back end </li>
-    	<li> Business policies </li>
-        <li> Security Statement </li>
-        <li> Site validation </li>
-        <li> CRUD operations </li>
-        <li> Integration of a mobile view </li>
-        <li> General presentation </li>
-    	</ul>
-  
-    </div>
-    <br/>
-    
-    <div class="col-sm-6">
-    
-	<h3>Joseph</h3>
-    <ul> 
-    	<li> PHP back end </li>
-    	<li> Business policies </li>
-    	<li> Site validation </li>
-        <li> General presentation </li>
-    </li>
-    </div>
-    <div class="col-sm-6">
-    <h3>Shayla</h3>
-    <ul>
-    	<li> PHP back end </li>
-    	<li> Integration of SEO</li>
-        <li> Integration of Google Analytics </li>
-        <li> Testing requirements </li>
-        <li> Integration of a mobile view </li>
-        <li> General presentation </li>
-        </ul>
-    
-</ul>
+                    <div class="col-sm-6">
+                    	<h3>Valery</h3>
+                        <ul>
+                        	<li> <strong> Original creater of site </strong> </li>
+                            <li> PHP back end </li>
+                            <li> Interaction with database </li>
+                            <li> Guest, Unprivilided, Privileged and Admin access </li>
+                            <li> Rating system </li>
+                            <li> Site validation </li>
+                            <li> CRUD operations </li>
+                            <li> Integration of a mobile view </li>
+                            <li> General presentation </li>
+                        </ul>
+                      
+                    </div>
+                        
+                    <div class="col-sm-6">
+                    	<h3>Tim</h3>
+                        <ul>
+                        	<li> <strong> Team leader </strong> </li>
+                        	<li> PHP back end </li>
+                            <li> Integration of site search </li>
+                            <li> Interaction with database </li>
+                            <li> Guest, Unprivilided, Privileged and Admin access </li>
+                            <li> Rating system </li>
+                            <li> Site validation </li>
+                        	<li> CRUD operations </li>
+                            <li> Integration of a mobile view </li>
+                            <li> General presentation </li>
+                        </ul>
+                        
+                    </div>
+                        
+                        <br/>
+                        
+                    <div class="col-sm-6">
+                    	<h3>Logan</h3>
+                        <ul> 
+                        	<li> PHP back end </li>
+                        	<li> Interaction with database </li>
+                            <li> Site validation </li>
+                            <li> CRUD operations </li>
+                            <li> Integration of a mobile view </li>
+                            <li> General presentation </li>
+                        </ul>
+                     
+                    </div>
+                        
+                    <div class="col-sm-6">
+                    	<h3>Jackie</h3>
+                        <ul>
+                        	<li> PHP back end </li>
+                        	<li> Business policies </li>
+                            <li> Security Statement </li>
+                            <li> Site validation </li>
+                            <li> CRUD operations </li>
+                            <li> Integration of a mobile view </li>
+                            <li> General presentation </li>
+                        </ul>
+                      
+                    </div>
+                        <br/>
+                        
+                    <div class="col-sm-6"> 
+                    	<h3>Joseph</h3>
+                        <ul> 
+                        	<li> PHP back end </li>
+                        	<li> Business policies </li>
+                        	<li> Site validation </li>
+                            <li> General presentation </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <h3>Shayla</h3>
+                        <ul>
+                        	<li> PHP back end </li>
+                        	<li> Integration of SEO</li>
+                            <li> Integration of Google Analytics </li>
+                            <li> Testing requirements </li>
+                            <li> Integration of a mobile view </li>
+                            <li> General presentation </li>
+                        </ul>
+                    </div>
+                </div>    
+            </div>    
+        </div>
 </div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
