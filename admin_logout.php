45e13800697f5ad71d676de3670aaa74e099a1c2
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-11-16 23:41:49 -0500

    Page Validation

--- admin_logout.php
+++ admin_logout.php
@@ -2,4 +2,10 @@
 session_start();
 unset($_SESSION['login']);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Admin Logout</title>
 <script>window.location.href = "admin_login.php";</script>
+</head>
+</html>
